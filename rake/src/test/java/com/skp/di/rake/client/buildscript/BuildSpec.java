1c0064594cec361f26003a797c01d748b441b44e
1ambda <1amb4a@gmail.com>
2015-05-23 01:24:59 +0900

    version upgrade script added
    - BuildScriptSpec 에 버전 포맷 `android_` 검증 추가
    - Gradle 에서 버전 자동으로 업그레이드
    - Gradle 에서 업그레이드된 버전 검증

--- rake/src/test/java/com/skp/di/rake/client/buildscript/BuildSpec.java
+++ rake/src/test/java/com/skp/di/rake/client/buildscript/BuildSpec.java
@@ -1,5 +1,7 @@
 package com.skp.di.rake.client.buildscript;
 
+import com.skp.di.rake.client.config.RakeMetaConfig;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
@@ -9,6 +11,7 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(RobolectricTestRunner.class)
 public class BuildSpec {
@@ -22,5 +25,12 @@ public class BuildSpec {
 
         Collections.sort(arr, (str1, str2) -> str1.compareTo(str2));
         assertEquals("a", arr.get(0));
+    }
+
+    @Test
+    public void testRakeVersionShouldStartWith() {
+        /* android_\d.\d.\d */
+
+        assertTrue(RakeMetaConfig.RAKE_CLIENT_VERSION.startsWith("android_"));
     }
 }
