e4764961934ba0fa68584e2533a9b4569ec0ef0e
1ambda <1amb4a@gmail.com>
2015-06-12 01:25:09 +0900

    replace Collections.synchronizedMap into ConcurrentHashMap

--- rake/src/main/java/com/skp/di/rake/client/utils/RakeLoggerFactory.java
+++ rake/src/main/java/com/skp/di/rake/client/utils/RakeLoggerFactory.java
@@ -5,13 +5,12 @@ import com.skp.di.rake.client.api.RakeUserConfig;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class RakeLoggerFactory {
 
-    static private HashMap<Class, RakeLogger> loggerMap =
-            new LinkedHashMap<Class, RakeLogger>();
-
-    static { Collections.synchronizedMap(loggerMap); }
+    static private Map<Class, RakeLogger> loggerMap = new ConcurrentHashMap<Class, RakeLogger>();
 
     static public RakeLogger getLogger(Class clazz, RakeUserConfig config) {
         synchronized (loggerMap) {
